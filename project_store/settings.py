4c916e0a0ee2133ec763eedbe85416a5ecf47ae1
Naveenpaul9940 <naveen.r.2021.ad@ritchennai.edu.in>
2025-12-27 14:24:58 +0530

    changed database configuration for effective storage

--- project_store/settings.py
+++ project_store/settings.py
@@ -54,8 +54,12 @@ WSGI_APPLICATION = "project_store.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("POSTGRES_DB", "projectstore"),
+        "USER": os.getenv("POSTGRES_USER", "postgres"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD", "your_postgres_password"),
+        "HOST": os.getenv("POSTGRES_HOST", "localhost"),
+        "PORT": os.getenv("POSTGRES_PORT", "5432"),
     }
 }
 
